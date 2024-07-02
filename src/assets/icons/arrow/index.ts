27cb3f30ff056284936572d2759732feba5c23d3
Sagar-v4 <sagarvariya4@gmail.com>
2024-07-02 15:56:54 +0530

    feat(ui): add new components
    added new form and select component from shadcn and up down icon

--- src/assets/icons/arrow/index.ts
+++ src/assets/icons/arrow/index.ts
@@ -1,4 +1,5 @@
 export * from "@/nextjs/assets/icons/arrow/chevron-down";
 export * from "@/nextjs/assets/icons/arrow/chevron-up";
 export * from "@/nextjs/assets/icons/arrow/chevron-right";
+export * from "@/nextjs/assets/icons/arrow/chevrons-up-down";
 export * from "@/nextjs/assets/icons/arrow/refresh-cw";
--- src/lib/redux/features/index.ts
+++ src/lib/redux/features/index.ts
@@ -1,7 +1,7 @@
+import ChartTimeListReducer from "@/lib/redux/features/chart/time/list";
 import themeReducer from "@/nextjs/lib/redux/features/theme/slice";
-
-export * from "@/nextjs/lib/redux/features/theme/slice";
 
 export const reducers = {
   theme: themeReducer,
+  chartTimeList: ChartTimeListReducer,
 };
