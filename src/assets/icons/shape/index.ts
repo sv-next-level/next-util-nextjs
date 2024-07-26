88bb6b2eba2d50bd2a96b7d3fd6ce4897fcd949f
Sagar-v4 <sagarvariya4@gmail.com>
2024-07-26 15:27:30 +0530

    feat: add elipsis icons

--- src/assets/icons/shape/index.ts
+++ src/assets/icons/shape/index.ts
@@ -1,4 +1,6 @@
 export * from "@/nextjs/assets/icons/shape/cross";
 export * from "@/nextjs/assets/icons/shape/dot";
+export * from "@/nextjs/assets/icons/shape/ellipsis-horizontal";
+export * from "@/nextjs/assets/icons/shape/ellipsis-vertical";
 export * from "@/nextjs/assets/icons/shape/plus";
 export * from "@/nextjs/assets/icons/shape/star";
