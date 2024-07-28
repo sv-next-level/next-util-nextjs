2d5124762d7185dbc0032d6d0a16f3f66465ee18
Sagar-v4 <sagarvariya4@gmail.com>
2024-07-28 15:46:05 +0530

    fix: drawer header text alignment

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -59,7 +59,7 @@ const DrawerHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("grid gap-1.5 p-4 text-left", className)} {...props} />
+  <div className={cn("grid gap-1.5 p-4 text-center", className)} {...props} />
 );
 DrawerHeader.displayName = "DrawerHeader";
 
